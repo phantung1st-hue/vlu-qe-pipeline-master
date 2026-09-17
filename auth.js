a8e126b3e5e93c9ade0a35bd8fd68395baa9472f
phantung1st-hue <phantung1st@gmail.com>
2026-09-17 16:58:33 +0700

    Break login logic for fail fast test

--- auth.js
+++ auth.js
@@ -13,7 +13,7 @@ function login(username, password, isLocked = false) {
         return false;
     }
 
-    return username === 'admin' && password === '123';
+    return username === 'admin' && password === '9999';
 }
 
 module.exports = { login };
